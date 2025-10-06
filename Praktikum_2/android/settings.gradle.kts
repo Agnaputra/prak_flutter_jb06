b85dbed09717be9fc2cfa36b0d1caa5b8c4c99b9
nanaputra <agenaputra@gmail.com>
2025-10-06 11:39:09 +0700

    push

--- Praktikum_2/android/settings.gradle.kts
+++ Praktikum_2/android/settings.gradle.kts
@@ -22,6 +22,5 @@ plugins {
     id("com.android.application") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
 }
-rootProject.name = "Praktikum1"
 
 include(":app")
